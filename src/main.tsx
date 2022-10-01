45d70d9e5601ae520087a7e9e550f78ded6a73bc
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-10-01 20:58:25 +0300

    Fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
 import './index.css';
 
 // eslint-disable-next-line unicorn/prefer-query-selector
