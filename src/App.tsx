392ef6f606343ea644e1714f25697bf815ed1659
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-10-01 19:08:12 +0300

    Implemented seismic activity display

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
+import { Fragment, Suspense, useRef, useState } from 'react';
 import { MoonScene } from './MoonScene';
 
 function App() {
+	const position = useRef({ top: -1, left: -1 });
+	const [selectedItemIndex, setSelectedItemIndex] = useState(-1);
+
+	const onSelectedChange = (selectedIndex: number, newPosition: { top: number; left: number }) => {
+		setSelectedItemIndex(selectedIndex);
+		position.current = newPosition;
+	};
+
 	return (
-		<Canvas>
-			<Suspense fallback={null}>
-				<MoonScene />
-			</Suspense>
-		</Canvas>
+		<Fragment>
+			<div
+				style={{
+					position: 'absolute',
+					top: position.current.top,
+					left: position.current.left,
+					background: 'red',
+					zIndex: 1000,
+				}}
+			>
+				Helo world
+			</div>
+			<Canvas>
+				<Suspense fallback={null}>
+					<MoonScene
+						points={[
+							{
+								lat: 0,
+								latError: 0,
+								lon: 0,
+								lonError: 0,
+								amplitude: 10,
+							},
+							{
+								lat: 10,
+								latError: 0,
+								lon: 10,
+								lonError: 0,
+								amplitude: 10,
+							},
+						]}
+						onSelectedItemChange={onSelectedChange}
+					/>
+				</Suspense>
+			</Canvas>
+		</Fragment>
 	);
 }
 
